6e993d18a842d28d56853f0067b51f8d5b1cc6c8
Sopee Thong <tsopee@gmail.com>
2021-03-20 20:41:12 -0400

    added random lower-case function

--- Develop/script.js
+++ Develop/script.js
@@ -13,6 +13,11 @@ function writePassword() {
 var userChoice = prompt("How many characters would you like your password to contain?");
 console.log("userChoice: " + userChoice);
 
+function lowerCase() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+
+}
+
 function generatePassword() {
 //TODO: Write the code that asks user abt the parameters for their pw
 //TODO:Use those Parameters to generate a new password
